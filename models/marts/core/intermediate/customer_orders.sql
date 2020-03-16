8ff405a5d6993f13ad00e47efb8ba0e5507850f2
Jonathan Natkins <jon.natkins@gmail.com>
2020-03-16 15:59:37 +0000

    Add customer lifetime to customer_orders model

--- models/marts/core/intermediate/customer_orders.sql
+++ models/marts/core/intermediate/customer_orders.sql
@@ -6,16 +6,21 @@ with orders as (
 
 final as (
 
-    select
-        customer_id,
+    select first_order,
+           most_recent_order,
+           CURRENT_DATE - first_order as customer_lifetime,
+           number_of_orders
+    FROM (
+        select
+            customer_id,
 
-        min(order_date) as first_order,
-        max(order_date) as most_recent_order,
-        count(order_id) as number_of_orders
-    from orders
+            min(order_date) as first_order,
+            max(order_date) as most_recent_order,
+            count(order_id) as number_of_orders
+        from orders
 
-    group by 1
-
+        group by 1
+    )
 )
 
 select * from final
